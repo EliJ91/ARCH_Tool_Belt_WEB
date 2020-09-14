71da5c1176579460ff90fd549801407624a22f70
Eliashib <59631669+EliJ91@users.noreply.github.com>
2020-09-13 20:21:57 -0400

    Add .env and node_modules to .gitignore

--- src/components/frontPage/frontPage.js
+++ src/components/frontPage/frontPage.js
@@ -1,7 +1,6 @@
 import React, {useEffect,useState,} from 'react'
 import axios from 'axios'
 import styled from 'styled-components'
-import '../../index.css'
 
 const Container = styled.table`
     border:3px solid white;
--- src/components/recentZvz/recentZvz.js
+++ src/components/recentZvz/recentZvz.js
@@ -1,7 +1,6 @@
 import React, {useState,useEffect} from 'react';
 import axios from 'axios';
 import styled from 'styled-components'
-import '../../index.css'
 
 const TableContainer = styled.table`
     display: grid;
